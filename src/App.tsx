8a05008c4cbe95a23780749916a330b72fb297fa
Skeller1z <kaktv1333@gmail.com>
2023-08-03 12:04:18 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Router from './Routes/routes';
+import { useServiceWorker } from './useServiceWorker';
+import Swal from 'sweetalert2';
+import PopupTopupSuccess from './components/PopupTopupSuccess';
+import { useNavigate } from 'react-router-dom';
 function App() {
+  const { waitingWorker, showReload, reloadPage } = useServiceWorker();
+  const [toggle, settoggle] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+useEffect(() => {
+  if (showReload && waitingWorker) {
+    Swal.fire({
+      title: 'Found a new Version!',
+      text: 'New Update Available!',
+      icon: 'success',
+      confirmButtonText: 'Update now!'
+    }).then(() => {
+      reloadPage()
+    })
+  }  else {
+    console.log("No update available!");
+  }
+}, [waitingWorker, showReload, reloadPage]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router />
+    </>
   );
 }
 
 export default App;
+
